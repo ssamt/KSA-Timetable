aa06a7783cac695df88cecb49844654803786aeb
김병권 <ssamtkwon@naver.com>
2020-12-23 18:40:41 +0900

    changed ALLOWED_HOSTS

--- Timetable/settings.py
+++ Timetable/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
