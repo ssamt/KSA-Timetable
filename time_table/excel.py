7551997273ee4c12b218c444ff0adf92e5e69f22
김병권 <ssamtkwon@naver.com>
2021-03-09 15:35:11 +0900

    lowered saturation for colors

--- time_table/excel.py
+++ time_table/excel.py
@@ -1,6 +1,7 @@
 import io
 import xlsxwriter
 from django.utils.translation import ugettext
+import colorsys
 
 
 RAW_LEN = 11
@@ -26,7 +27,32 @@ colors = [
     '#A0E0E0', '#E0A0E0', '#E0E0A0',
     '#FFC0C0', '#C0FFC0', '#C0C0FF',
     '#FFFFC0', '#FFC0FF', '#C0FFFF',
-]
+]  # the colors are modified below
+
+
+# https://stackoverflow.com/questions/214359/converting-hex-color-to-rgb-and-vice-versa
+def hex_to_rgb(value):
+    """Return (red, green, blue) for the color given as #rrggbb."""
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+
+def rgb_to_hex(red, green, blue):
+    """Return color as #rrggbb for the given color values."""
+    return '#%02x%02x%02x' % (int(red), int(green), int(blue))
+
+
+def dec_sat(color, factor):
+    hsv = list(colorsys.rgb_to_hsv(*hex_to_rgb(color)))
+    hsv[1] *= factor
+    print(tuple(hsv))
+    new = rgb_to_hex(*colorsys.hsv_to_rgb(*tuple(hsv)))
+    return new
+
+
+for i in range(len(colors)):
+    colors[i] = dec_sat(colors[i], 0.83)
 
 
 def has_meal(period):
