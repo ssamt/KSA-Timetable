6063ca0ce705b33e9ea19c469df8364960a959bd
ssamt <73675935+ssamt@users.noreply.github.com>
2021-04-18 18:34:39 +0900

    added feature for pasting timetable in Firefox

--- time_table/excel.py
+++ time_table/excel.py
@@ -3,11 +3,12 @@ import xlsxwriter
 from django.utils.translation import ugettext
 import colorsys
 
-
 RAW_LEN = 11
 NAME_R, TEACHER_R, TIME_R, CLASS_NUM_R = 3, 6, 7, 8  # index in raw string
-LECTURE_LEN = 4
-NAME_S, TEACHER_S, TIME_S, CLASS_NUM_S = 0, 1, 2, 3  # index in readable string
+TABLE_LEN = 5
+NAME_T, CLASS_NUM_T, TEACHER_T, CLASSROOM_T = 0, 2, 3, 4  # index in raw table
+LECTURE_LEN = [4, 5]
+NAME_S, TEACHER_S, TIME_S, CLASS_NUM_S, CLASSROOM_S = 0, 1, 2, 3, 4  # index in readable string
 NAME_X, CLASSROOM_X, CLASS_NUM_X, TEACHER_X = 0, 1, 2, 3  # index in excel data sheet
 LINK_START_COLUMN = 4
 days = ['월', '화', '수', '목', '금']
@@ -16,6 +17,7 @@ class_time = [
     '13:40~14:30', '14:40~15:30', '15:40~16:30', '16:40~17:30', '17:40~18:30',
     '19:30~20:20', '20:30~21:20', '21:30~22:20',
 ]
+class_postfix = '분반'
 aa_time = '8:30~8:50'
 aa_name = 'AA 모임'
 aa_color = '#D0D0D0'
@@ -85,9 +87,11 @@ for i in range(1, len(class_time)):
 example_text = ['교과명', '분반', '교원', '교실']
 
 
-# takes the raw string copied from https://students.ksa.hs.kr/
-# returns a more readable string
-def raw_to_str(data):
+def data_is_list(data):
+    return data.count('\n') < 25
+
+
+def raw_list_to_string(data):
     data = data.strip()
     data = data.splitlines()
     for i in range(len(data)):
@@ -98,10 +102,45 @@ def raw_to_str(data):
             data[i][j] = data[i][j].strip()
         data[i][TIME_R] = data[i][TIME_R].split('|')
         data[i][TIME_R] = '/'.join(data[i][TIME_R])
-        data[i][CLASS_NUM_R] += '반'
+        data[i][CLASS_NUM_R] += class_postfix
         data[i] = ', '.join([data[i][NAME_R], data[i][TEACHER_R], data[i][TIME_R], data[i][CLASS_NUM_R]])
     data = '\n'.join(data)
     return data
+
+
+def raw_table_to_string(data):
+    arr = []
+    added = set()
+    data = data.strip()
+    data = data.split('교시')
+    for i in range(1, len(data)):
+        data[i] = data[i][:data[i].rfind('\n')]
+        data[i] = data[i].split('\t')
+        for j in range(0, len(data[i])):
+            data[i][j] = data[i][j].strip()
+            if data[i][j] != '':
+                data[i][j] = data[i][j].splitlines()
+                if data[i][j][NAME_T] not in added:
+                    added.add(data[i][j][NAME_T])
+                    arr.append([data[i][j][NAME_T], data[i][j][TEACHER_T], [], data[i][j][CLASS_NUM_T],
+                                data[i][j][CLASSROOM_T]])
+                for k in range(len(arr)):
+                    if arr[k][NAME_S] == data[i][j][NAME_T]:
+                        arr[k][TIME_S].append(f'{days[j-1]}{i}')
+    for i in range(len(arr)):
+        arr[i][TIME_S] = '/'.join(arr[i][TIME_S])
+        arr[i] = ', '.join(arr[i])
+    arr = '\n'.join(arr)
+    return arr
+
+
+# takes the raw string copied from https://students.ksa.hs.kr/
+# returns a more readable string
+def raw_to_str(data):
+    if data_is_list(data):
+        return raw_list_to_string(data)
+    else:
+        return raw_table_to_string(data)
 
 
 class Lecture:
@@ -113,17 +152,23 @@ class Lecture:
         data[TIME_S] = data[TIME_S].split('/')
         for i in range(len(data[TIME_S])):
             data[TIME_S][i] = [days.index(data[TIME_S][i][0]), int(data[TIME_S][i][1:])]
-        if not data[CLASS_NUM_S].isdigit():  # '반' 제거
-            data[CLASS_NUM_S] = data[CLASS_NUM_S][:-1]
+        if not data[CLASS_NUM_S].isdigit():
+            data[CLASS_NUM_S] = data[CLASS_NUM_S][:-len(class_postfix)]
         self.name = data[NAME_S]
         self.teacher = data[TEACHER_S]
         self.time = data[TIME_S]
         self.class_num = data[CLASS_NUM_S]
+        if len(data) > CLASSROOM_S:
+            self.classroom = data[CLASSROOM_S]
+        else:
+            self.classroom = None
 
     # reverses __init__
     def __str__(self):
         string = [self.name, self.teacher, self.time, self.class_num]
-        string[CLASS_NUM_S] += '반'
+        if self.classroom:
+            string.append(self.classroom)
+        string[CLASS_NUM_S] += class_postfix
         for i in range(len(string[TIME_S])):
             string[TIME_S][i] = days[string[TIME_S][i][0]] + str(string[TIME_S][i][1])
         string[TIME_S] = '/'.join(string[TIME_S])
@@ -148,9 +193,9 @@ def link_cells(row, link_num):
 
 # remove digits at the end of string
 def remove_end_num(string):
-    for i in range(len(string)-1, -1, -1):
+    for i in range(len(string) - 1, -1, -1):
         if not ord('0') <= ord(string[i]) <= ord('9'):
-            return string[:i+1]
+            return string[:i + 1]
     return ''
 
 
@@ -171,7 +216,7 @@ def apply_basic_format(format):
 
 
 def class_text(class_num):
-    return f'{class_num}분반'
+    return f'{class_num}{class_postfix}'
 
 
 class Table:
@@ -212,7 +257,7 @@ class Table:
                 add = 2
             else:
                 add = 1
-        return self.period_row_list()[period]+add
+        return self.period_row_list()[period] + add
 
     def meal_row(self, meal):
         add = 0
@@ -221,7 +266,7 @@ class Table:
                 add = 2
             else:
                 add = 1
-        return self.meal_row_list()[meal]+add
+        return self.meal_row_list()[meal] + add
 
     # returns excel file data
     def get_excel(self):
@@ -246,10 +291,10 @@ class Table:
             apply_basic_format(example_period_format[i])
             example_period_format[i].set_bg_color(colors[0])
         example_period_format[0].set_bottom(0)
-        for i in range(1, len(example_text)-1):
+        for i in range(1, len(example_text) - 1):
             example_period_format[i].set_top(0)
             example_period_format[i].set_bottom(0)
-        example_period_format[len(example_text)-1].set_top(0)
+        example_period_format[len(example_text) - 1].set_top(0)
 
         if self.use_link:
             example_link_format = workbook.add_format()
@@ -274,7 +319,7 @@ class Table:
         # set column and row size, merge cells
         table.set_column(0, 0, 15)
         table.set_column(1, 1, 5)
-        table.set_column(2, 2+len(days)-1, 15)
+        table.set_column(2, 2 + len(days) - 1, 15)
         table.set_row(0, 20)
         if self.include_aa:
             if self.use_link:
@@ -282,40 +327,40 @@ class Table:
                 table.set_row(2, 20)
                 table.merge_range(1, 0, 2, 0, '')
                 table.merge_range(1, 1, 2, 1, '')
-                table.merge_range(1, 2, 1, 2+len(days)-1, '')
-                table.merge_range(2, 2, 2, 2+len(days)-1, '')
+                table.merge_range(1, 2, 1, 2 + len(days) - 1, '')
+                table.merge_range(2, 2, 2, 2 + len(days) - 1, '')
             else:
                 table.set_row(1, 80)
-                table.merge_range(1, 2, 1, 2+len(days)-1, '')
+                table.merge_range(1, 2, 1, 2 + len(days) - 1, '')
         for i in range(1, len(self.period_row_list())):
             if self.use_link:
                 table.set_row(self.period_row(i), 80)
-                table.set_row(self.period_row(i)+1, 20)
-                table.merge_range(self.period_row(i), 0, self.period_row(i)+1, 0, '')
-                table.merge_range(self.period_row(i), 1, self.period_row(i)+1, 1, '')
+                table.set_row(self.period_row(i) + 1, 20)
+                table.merge_range(self.period_row(i), 0, self.period_row(i) + 1, 0, '')
+                table.merge_range(self.period_row(i), 1, self.period_row(i) + 1, 1, '')
             else:
                 table.set_row(self.period_row(i), 80)
         for i in range(len(self.meal_row_list())):
             table.set_row(self.meal_row(i), 40)
-            table.merge_range(self.meal_row(i), 0, self.meal_row(i), 2+len(days)-1, '')
+            table.merge_range(self.meal_row(i), 0, self.meal_row(i), 2 + len(days) - 1, '')
 
         # applying basic format to all cells in 시간표
         if self.use_link:
-            row_num = 2*(len(class_time)-1)+len(meals)+1
+            row_num = 2 * (len(class_time) - 1) + len(meals) + 1
             if self.include_aa:
                 row_num += 2
         else:
-            row_num = (len(class_time)-1)+len(meals)+1
+            row_num = (len(class_time) - 1) + len(meals) + 1
             if self.include_aa:
                 row_num += 1
         for i in range(row_num):
-            for j in range(len(days)+2):
+            for j in range(len(days) + 2):
                 table.write(i, j, '', table_format)
 
         # write data to excel
         if self.include_aa:
             table.write(1, 0, ugettext(aa_time), table_format)
-            aa_data_row = len(self.lec)+2
+            aa_data_row = len(self.lec) + 2
             table.write_formula(1, 2,
                                 period_formula([f'데이터!A{aa_data_row}', f'데이터!C{aa_data_row}',
                                                 f'데이터!D{aa_data_row}', f'데이터!B{aa_data_row}']),
@@ -329,64 +374,70 @@ class Table:
         for i in range(len(self.meal_row_list())):
             table.write(self.meal_row(i), 0, ugettext(meals[i][1]), table_format)
         for i in range(len(days)):
-            table.write(0, i+2, ugettext(days[i]), table_format)
+            table.write(0, i + 2, ugettext(days[i]), table_format)
         for i in range(len(self.lec)):
             for j in range(len(self.lec[i].time)):
-                table.write_formula(self.period_row(self.lec[i].time[j][1]), self.lec[i].time[j][0]+2,
-                                    period_formula([f'데이터!A{i+2}', f'데이터!C{i+2}', f'데이터!D{i+2}', f'데이터!B{i+2}']),
-                                    cell_format=period_format[i],
-                                    value=f'{self.lec[i].name}\r\n{class_text(self.lec[i].class_num)}\r\n{self.lec[i].teacher}')
+                preview = f'{self.lec[i].name}\r\n{class_text(self.lec[i].class_num)}\r\n{self.lec[i].teacher}'
+                if self.lec[i].classroom:
+                    preview += f'\r\n{self.lec[i].classroom}'
+                table.write_formula(self.period_row(self.lec[i].time[j][1]), self.lec[i].time[j][0] + 2,
+                                    period_formula(
+                                        [f'데이터!A{i + 2}', f'데이터!C{i + 2}', f'데이터!D{i + 2}', f'데이터!B{i + 2}']),
+                                    cell_format=period_format[i], value=preview)
                 if self.use_link:
-                    table.write_formula(self.period_row(self.lec[i].time[j][1])+1, self.lec[i].time[j][0]+2,
-                                        link_formula(link_cells(i+2, len(self.link))),
+                    table.write_formula(self.period_row(self.lec[i].time[j][1]) + 1, self.lec[i].time[j][0] + 2,
+                                        link_formula(link_cells(i + 2, len(self.link))),
                                         cell_format=link_format[i], value='')
 
         # create 데이터 worksheet
         data = workbook.add_worksheet('데이터')
-        example_row = len(self.lec)+1  # row and column that the explanation begins
+        example_row = len(self.lec) + 1  # row and column that the explanation begins
         if self.include_aa:
             example_row += 1
         if self.use_link:
-            example_column = 4+len(self.link)
+            example_column = 4 + len(self.link)
         else:
             example_column = 4
         data.set_column(example_column, example_column, 15)
         vcenter_format = workbook.add_format()
         vcenter_format.set_align('vcenter')
         if self.use_link:
-            for i in range(len(example_text)+1):
-                data.set_row(example_row+i, 80/len(example_text)+1)
+            for i in range(len(example_text) + 1):
+                data.set_row(example_row + i, 80 / len(example_text) + 1)
         else:
             for i in range(len(example_text)):
-                data.set_row(example_row+i, 80/len(example_text))
+                data.set_row(example_row + i, 80 / len(example_text))
         data.write(0, NAME_X, ugettext('교과명'))
         data.write(0, CLASSROOM_X, ugettext('교실'))
         data.write(0, CLASS_NUM_X, ugettext('분반'))
         data.write(0, TEACHER_X, ugettext('교원'))
         if self.use_link:
             for i in range(len(self.link)):
-                data.write(0, LINK_START_COLUMN+i, ugettext(self.link[i]))
+                data.write(0, LINK_START_COLUMN + i, ugettext(self.link[i]))
         for i in range(len(self.lec)):
             data.write(i+1, NAME_X, ugettext(self.lec[i].name))
             data.write(i+1, CLASS_NUM_X, ugettext(class_text(self.lec[i].class_num)))
             data.write(i+1, TEACHER_X, ugettext(self.lec[i].teacher))
+            if self.lec[i].classroom:
+                data.write(i+1, CLASSROOM_X, ugettext(self.lec[i].classroom))
         if self.include_aa:
-            data.write(len(self.lec)+1, NAME_X, ugettext(aa_name))
+            data.write(len(self.lec) + 1, NAME_X, ugettext(aa_name))
         for i in range(len(example_text)):
-            data.write(example_row+i, example_column, ugettext(example_text[i]), example_period_format[i])
-        data.write(example_row, example_column+1, ugettext('이 워크시트의 데이터로 시간표가 만들어짐'), vcenter_format)
-        data.write(example_row+3, example_column+1, ugettext('<- 왼쪽 표에 교실을 입력하면 자동으로 생성됨'), vcenter_format)
+            data.write(example_row + i, example_column, ugettext(example_text[i]), example_period_format[i])
+        data.write(example_row, example_column + 1, ugettext('이 워크시트의 데이터로 시간표가 만들어짐'), vcenter_format)
+        data.write(example_row + 3, example_column + 1, ugettext('<- 왼쪽 표에 교실을 입력하면 자동으로 생성됨'), vcenter_format)
         if self.use_link:
-            data.write(example_row+len(example_text), example_column, ugettext('Link'), example_link_format)
-            data.write(example_row+len(example_text), example_column+1, ugettext(f'<- {", ".join(self.link)} 밑에 링크를 입력하면 생성됨'),
+            data.write(example_row + len(example_text), example_column, ugettext('Link'), example_link_format)
+            data.write(example_row + len(example_text), example_column + 1,
+                       ugettext(f'<- {", ".join(self.link)} 밑에 링크를 입력하면 생성됨'),
                        vcenter_format)
         if self.use_link:
-            start_row = example_row+len(example_text)+1
+            start_row = example_row + len(example_text) + 1
         else:
-            start_row = example_row+len(example_text)
-        data.write(start_row, example_column+1, ugettext('https://ksatimetable.herokuapp.com'))
-        data.write(start_row+1, example_column+1, ugettext('버그, 문의사항 등은 20-017 김병권'))
-        data.write(start_row+2, example_column+1, ugettext(f'key: {self.key}'))
+            start_row = example_row + len(example_text)
+        data.write(start_row, example_column + 1, ugettext('https://ksatimetable.herokuapp.com'))
+        data.write(start_row + 1, example_column + 1, ugettext('버그, 문의사항 등은 20-017 김병권'))
+        data.write(start_row + 2, example_column + 1, ugettext(f'key: {self.key}'))
         workbook.close()
         excel_data = output.getvalue()
         return excel_data
--- time_table/forms.py
+++ time_table/forms.py
@@ -1,55 +1,40 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from .excel import RAW_LEN, TIME_R, CLASS_NUM_R, LECTURE_LEN, TIME_S, CLASS_NUM_S, days
-
-raw_data_example = '''예시
-1 	수리정보과학부 	자연계열교과핵심 	수학2 	4 	1 	정명주 	월3|월4|화2|수1|목5 	3 	18 	15
-2 	화학생물학부 	자연계열교과핵심 	생물학및실험2 	3 	1 	권창섭 	월5|수2|목1 	6 	18 	16
-3 	인문예술학부 	인문계열교과핵심 	한국사의이해 	3 	1 	강재순 	월6|목2|금1 	3 	18 	15'''
-raw_data_short_example = raw_data_example.splitlines()[1]
-
-lecture_data_example = '''예시
-수학2, 정명주, 월3/월4/화2/수1/목5, 3반
-생물학및실험2, 권창섭, 월5/수2/목1, 6반
-한국사의이해, 강재순, 월6/목2/금1, 3반'''
+from .excel import RAW_LEN, TIME_R, CLASS_NUM_R, \
+    TABLE_LEN, NAME_T, CLASS_NUM_T, TEACHER_T, CLASSROOM_T, \
+    LECTURE_LEN, TIME_S, CLASS_NUM_S, days, data_is_list, class_postfix
 
 
 raw_error_message = [
-    '{row}번째 줄이 형식에 맞지 않습니다. 예시와 형식이 같은지 확인해주세요.',
-    f'예시: {raw_data_short_example}',
-    '주의: 각 항목은 2개 이상의 공백이나 탭으로 구분되어 있어야 합니다.',
+    '입력이 형식에 맞지 않습니다. 밑의 지시사항을 읽어주세요.',
     '해결이 되지 않는다면 key 값을 가지고 20-017 김병권에게 연락해주세요.',
 ]
 
 
-def raw_error(row):
-    errors = raw_error_message.copy()
-    errors[0] = errors[0].format(row=row)
-    return ValidationError([ValidationError(message) for message in errors])
+raw_error = ValidationError([ValidationError(message) for message in raw_error_message])
 
 
 lecture_error_message = [
-    '{row}번째 줄이 형식에 맞지 않습니다.',
-    '"교과명, 교원, 수업 시간, 분반" 형식인지 확인해 주세요.',
+    '입력이 형식에 맞지 않습니다.',
+    '"교과명, 교원, 수업 시간, 분반" 또는',
+    '"교과명, 교원, 수업 시간, 분반, 교실" 형식인지 확인해 주세요.',
     '해결이 되지 않는다면 key 값을 가지고 20-017 김병권에게 연락해주세요.',
 ]
 
 
-def lecture_error(row):
-    errors = lecture_error_message.copy()
-    errors[0] = errors[0].format(row=row)
-    return ValidationError([ValidationError(message) for message in errors])
+lecture_error = ValidationError([ValidationError(message) for message in lecture_error_message])
 
 
 class RawForm(forms.Form):
-    raw_data = forms.CharField(widget=forms.Textarea(attrs={'placeholder': raw_data_example, 'rows': 15, 'cols': 150}),
+    raw_data = forms.CharField(widget=forms.Textarea(attrs={'rows': 15, 'cols': 150}),
                                label='',
                                help_text='''<br>
                                 <a href="https://students.ksa.hs.kr/" target="_blank">https://students.ksa.hs.kr/</a>
                                 > 수강정보 > 수강신청현황<br>
-                                본인의 수강신청과목 표를 붙여넣으면 됩니다.(머리글 행 제외)<br>''')
+                                본인의 수강신청과목 표를 붙여넣으면 됩니다.(머리글 행 제외)<br>
+                                Firefox 사용자는 수강시간표를 붙여넣으면 자동으로 교실도 입력됩니다.''')
 
-    def clean_raw_data(self):
+    def clean_raw_list(self):
         cleaned_data = self.cleaned_data
         data = cleaned_data.get('raw_data')
         data = data.strip()
@@ -59,21 +44,49 @@ class RawForm(forms.Form):
             data[i] = data[i].split('  ')
             data[i] = list(filter(None, data[i]))
             if len(data[i]) != RAW_LEN:
-                raise raw_error(i+1)
+                raise raw_error
             for j in range(len(data[i])):
                 data[i][j] = data[i][j].strip()
             data[i][TIME_R] = data[i][TIME_R].split('|')
             for j in range(len(data[i][TIME_R])):
                 if data[i][TIME_R][j][0] not in days or (not data[i][TIME_R][j][1:].isdigit()):
-                    raise raw_error(i+1)
+                    raise raw_error
             if not data[i][CLASS_NUM_R].isdigit():
-                raise raw_error(i+1)
+                raise raw_error
         return cleaned_data.get('raw_data')
+
+    def clean_raw_table(self):
+        cleaned_data = self.cleaned_data
+        data = cleaned_data.get('raw_data')
+        data = data.strip()
+        data = data.split('교시')
+        for i in range(1, len(data)):
+            if '\n' not in data[i] and data[i] != '':
+                raise raw_error
+            if i < len(data)-1:
+                data[i] = data[i][:data[i].rfind('\n')]
+            data[i] = data[i].split('\t')
+            if len(data[i]) > len(days)+1:
+                raise raw_error
+            for j in range(0, len(data[i])):
+                data[i][j] = data[i][j].strip()
+                if data[i][j] != '':
+                    data[i][j] = data[i][j].splitlines()
+                    if len(data[i][j]) != TABLE_LEN:
+                        raise raw_error
+        return cleaned_data.get('raw_data')
+
+    def clean_raw_data(self):
+        cleaned_data = self.cleaned_data
+        data = cleaned_data.get('raw_data')
+        if data_is_list(data):
+            return self.clean_raw_list()
+        else:
+            return self.clean_raw_table()
 
 
 class DataForm(forms.Form):
-    lecture_data = forms.CharField(widget=forms.Textarea(attrs={'placeholder': lecture_data_example,
-                                                                'rows': 15, 'cols': 50}),
+    lecture_data = forms.CharField(widget=forms.Textarea(attrs={'rows': 15, 'cols': 50}),
                                    label='수업 정보')
     use_link = forms.BooleanField(label='온라인 수업', required=False)
     include_aa = forms.BooleanField(label='AA 모임', required=False)
@@ -85,14 +98,14 @@ class DataForm(forms.Form):
         data = data.splitlines()
         for i in range(len(data)):
             data[i] = data[i].split(',')
-            if len(data[i]) != LECTURE_LEN:
-                raise lecture_error(i+1)
+            if len(data[i]) not in LECTURE_LEN:
+                raise lecture_error
             for j in range(len(data[i])):
                 data[i][j] = data[i][j].strip()
             data[i][TIME_S] = data[i][TIME_S].split('/')
             for j in range(len(data[i][TIME_S])):
                 if not (data[i][TIME_S][j][0] in days and data[i][TIME_S][j][1:].isdigit()):
-                    raise lecture_error(i+1)
-            if not (data[i][CLASS_NUM_S].isdigit() or data[i][CLASS_NUM_S][:-1].isdigit()):
-                raise lecture_error(i+1)
+                    raise lecture_error
+            if not (data[i][CLASS_NUM_S].isdigit() or data[i][CLASS_NUM_S][:-2].isdigit()):
+                raise lecture_error
         return clean_data.get('lecture_data')
